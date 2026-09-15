177f5f773f4468b625016fecce324c1d74ccf983
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-16 00:00:13 +0200

    Load event-wake diagnostics in bridge worker

--- chat_bridge/service_worker.js
+++ chat_bridge/service_worker.js
@@ -7,6 +7,7 @@ importScripts(
   "worker_transport.js",
   "worker_event_wake.js",
   "native_events.js",
+  "worker_event_diagnostics.js",
   "worker_controls.js",
   "worker_delivery.js",
   "worker_conversations.js",
